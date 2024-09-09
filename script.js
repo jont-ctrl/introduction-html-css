3935cf1668eeec927e9375335cfebf40da56841f
gragas <180638563+jont-ctrl@users.noreply.github.com>
2024-09-09 15:25:02 +0200

    Added spin button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+const spinBtn = document.querySelector('#spinBtn');
+const forestImg = document.querySelector('#forestImg');
+
+console.log('Hello World');
+
+spinBtn.addEventListener('click', () => {
+  // Needed short timer between remove and add for repeat animation to work
+  forestImg.classList.remove('forestImgClass');
+
+  setTimeout(() => {
+    forestImg.classList.add('forestImgClass');
+  }, 10);
+});
